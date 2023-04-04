6dbcf5affcb35fae9da39f620a14791e83733ca5
Zlikedokiller <73589522+Zlikedokiller@users.noreply.github.com>
2023-04-04 16:46:15 +0000

    Update thing.py

--- thing.py
+++ thing.py
@@ -1,16 +1,1 @@
-import sqlite3
-
-conn = sqlite3.connect('newdb.db')
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM users")
-results = cursor.fetchall()
-
-result = []
-
-def request():
-    for row in results:
-        results.append(row)
-
-conn.close()
-
-print(results)
+result = "day"
